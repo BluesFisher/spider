8aa130e560d6e8a49992c25bc6635f4772f8d717
adamsyu <adamsyu@tencent.com>
2020-04-15 09:40:12 +0800

    add sql

---
+++ common/sql_obj.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import MySQLdb
+
+
+class SqlObj(object):
+    def __init__(self,
+                 host='localhost',
+                 username='root',
+                 password='root',
+                 database='my_python'):
+        self.host = host
+        self.username = username
+        self.password = password
+        self.database = database
+        self.db = ''
+        self.cursor = ''
+
+    def connect_db(self):
+        # 打开数据库连接
+        self.db = MySQLdb.connect(self.host,
+                                  self.username,
+                                  self.password,
+                                  self.database,
+                                  charset='utf8')
+        self.cursor = self.db.cursor()
+        self.cursor.execute("SELECT VERSION()")
+        print "连接成功：Database version %s " % self.cursor.fetchone()
+
+    def get_cursor(self):
+        self.cursor = self.db.cursor()
+
+    def close_db(self):
+        self.cursor.close()
+        self.db.close()
+
+    def _deal_values(self, value):
+        """
+        self._deal_values(value) -> str or list
+            处理传进来的参数
+        """
+        # 如果是字符串则加上''
+        if isinstance(value, str):
+            value = ("'{value}'".format(value=value))
+        # 如果是字典则变成key=value形式
+        elif isinstance(value, dict):
+            result = []
+            for key, value in value.items():
+                value = self._deal_values(value)
+                res = "{key}={value}".format(key=key, value=value)
+                result.append(res)
+            return result
+        else:
+            value = (str(value))
+        return value
+
+    def insert(self, table, insert_data, is_replace=False):
+        '''
+            insert(table, insert_data)
+            添加数据到数据库
+            str -> table 为字符串
+            [{},{}] -> 为列表中嵌套字典类型
+        '''
+        self.connect_db()
+
+        print insert_data
+
+        try:
+            for data in insert_data:
+                key = ','.join(data.keys())
+                values = map(self._deal_values, data.values())
+                insert_data = ', '.join(values)
+
+                action = 'insert' if not is_replace else 'replace'
+                sql = (action + " into {table}({key}) values ({val})").format(
+                    table=table, key=key, val=insert_data)
+
+                print sql
+
+                self.cursor.execute(sql)
+                self.db.commit()
+
+                print 'sql insert ok: ', sql
+        except Exception as error:
+            print 'sql insert err: ', error
+        finally:
+            self.close_db()
+
+    def delete(self, table, condition):
+        '''
+            delete(table, condition)
+            删除数据库中的数据
+            str -> table 字符串类型
+            dict -> condition 字典类型
+        '''
+        self.connect_db()
+
+        try:
+            # 处理删除的条件
+            condition_list = self._deal_values(condition)
+            condition_data = ' and '.join(condition_list)
+
+            # 构建sql语句
+            sql = "delete from {table} where {condition}".format(
+                table=table, condition=condition_data)
+
+            self.cursor.execute(sql)
+            self.db.commit()
+
+            print 'sql delete ok: ', sql
+        except Exception as error:
+            print 'sql delete err: ', error
+        finally:
+            self.close_db()
+
+    def update(self, table, data, condition=None):
+        """
+            update(table, data, [condition])
+            更新数据
+            str -> table 字符串类型
+            dict -> data 字典类型
+            dict -> condition 字典类型
+        """
+        self.connect_db()
+
+        try:
+            # 处理传入的数据
+            update_list = self._deal_values(data)
+            update_data = ",".join(update_list)
+            # 判断是否有条件
+            if condition is not None:
+                # 处理传入的条件
+                condition_list = self._deal_values(condition)
+                condition_data = ' and '.join(condition_list)
+                sql = "update {table} set {values} where {condition}".format(
+                    table=table, values=update_data, condition=condition_data)
+            else:
+                sql = "update {table} set {values}".format(table=table,
+                                                           values=update_data)
+
+            self.cursor.execute(sql)
+            self.db.commit()
+
+            print 'sql update ok: ', sql
+        except Exception as error:
+            print 'sql update err: ', error
+        finally:
+            self.close_db()
+
+    def get(self, table, show_list=['*'], condition=None, get_one=False):
+        """
+            get(table, show_list, [condition, get_one]) -> tupe
+            获取数据 返回一个元祖
+            str -> table 字符串类型
+            list -> show_list 列表类型
+            dict -> condition 字典类型
+            boolean -> get_one 布尔类型
+        """
+        self.connect_db()
+        result = ()
+        try:
+            # 处理显示的数据
+            show_list = ",".join(show_list)
+            sql = "select {key} from {table}".format(key=show_list,
+                                                     table=table)
+            # 处理传入的条件
+            if condition:
+                condition_list = self._deal_values(condition)
+                condition_data = 'and'.join(condition_list)
+                sql = "select {key} from {table} where{condition}".format(
+                    key=show_list, table=table, condition=condition_data)
+
+            self.cursor.execute(sql)
+
+            result = self.cursor.fetchone(
+            ) if get_one else self.cursor.fetchall()
+
+            print 'sql get ok: ', sql
+        except Exception as error:
+            result = ()
+            print 'sql get err: ', error
+        finally:
+            self.close_db()
+            return result
+
+
+if __name__ == '__main__':
+    sql_obj = SqlObj()
+    sql_obj.insert('users', [{
+        'phone': '13612817761',
+        'password': '1234567',
+        'name': 'yu'
+    }])
+    # sql_obj.delete('users', {'phone': '13612817761'})
+    # sql_obj.update('users', {'name': 'yu'}, {'phone': '13612817761'})
+    # print sql_obj.get('users', ['name', 'phone'])
--- spiderNews/student_guide.py
+++ spiderNews/student_guide.py
@@ -15,6 +15,8 @@ import copy
 import time
 from common.json_func import JsonFunc
 from common_func import CommonFunc
+from common.sql_obj import SqlObj
+from common.utils import Utils
 
 list_items = {}
 
@@ -33,12 +35,39 @@ def get_info():
                      ensure_ascii=False), len(list_items.keys())
 
 
+def save_db(path):
+    try:
+        with open(path + '.json') as fp:
+            data = json.loads(fp.read())
+            sql_obj = SqlObj()
+            insert_data = []
+            for item in data.keys():
+                insert_data.append({
+                    'college':
+                    item.encode('utf8'),
+                    'url':
+                    data[item]['url'].encode('gbk'),
+                    'content':
+                    Utils().unicode_convert(
+                        json.dumps(data[item]['content'], ensure_ascii=False))
+                })
+
+            sql_obj.insert('t_student_guide', insert_data, True)
+        print 'save_db ok'
+    except Exception as error:
+        print 'save_db failed: ', error
+
+
 if __name__ == '__main__':
     path = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    date = time.strftime('%Y%m%d', time.localtime(time.time()))
+    file_path = path + '/news/student_guide_' + date
 
     get_info()
     date = time.strftime('%Y%m%d', time.localtime(time.time()))
-    JsonFunc().save_json(list_items, path + '/news/student_guid_' + date)
+    JsonFunc().save_json(list_items, file_path)
+
+    save_db(file_path)
 
     # url = 'http://www.gaokao.com/e/20200312/5e6a165d1060f.shtml' # img
     # url = 'http://www.gaokao.com/e/20200326/5e7c0b3a2aeca.shtml' # normal
