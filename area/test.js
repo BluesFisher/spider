cccade2fc009f9bb9de502f06949f5e68e66cb07
adamsyu <adamsyu@tencent.com>
2023-04-08 23:50:47 +0800

    feat: update

--- area/test.js
+++ area/test.js
@@ -146,52 +146,146 @@ const setMtElement = () => {
 // getMtData();
 // setMtElement()
 
+/**
+ * 获取大众点评篮球场详细信息，h5的headers
+ * @param {*} item
+ * @returns
+ */
+const getDzDetail = (item) =>
+  new Promise((resolve) => {
+    request.get(
+      `https://mapi.dianping.com/mapi/wechat/shop.bin?shopUuid=${item.shopUuid}&shopuuid=${item.shopUuid}`,
+      {
+        headers: {
+          Cookie:
+            "seouser_ab=ugcdetail%3AA%3A1; _lxsdk_cuid=18759a1439bc8-08b58ca907344d-1e525634-1ea000-18759a1439bc8; _lxsdk=18759a1439bc8-08b58ca907344d-1e525634-1ea000-18759a1439bc8; _hc.v=962e2f7f-8566-5bdc-3a6f-07457415888d.1680835955; WEBDFPID=zvw58853xw5u518vz5w5y704w1v0495w81240z85z7x979589wwy522z-1996195955101-1680835953717GGSAQEA75613c134b6a252faa6802015be905513328; fspop=test; cy=7; cye=shenzhen; _lx_utm=utm_source%3Dgoogle%26utm_medium%3Dorganic; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1680918479; s_ViewType=10; dper=563a4af6dd681d5a2dda291cad1aaff022d8d712f3de7a42344246637734a31c670e1bec687aa17aa5d22d7820834244aa9d3a9186c4ee0b73aaa03578a544e2; qruuid=402aca2a-bae9-4379-b121-e7e662400798; ll=7fd06e815b796be3df069dec7836c3df; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1680960824",
+          Host: "mapi.dianping.com",
+        },
+      },
+      (err, res) => {
+        if (!err && res.statusCode === 200) {
+          const { address, phoneNos, geoPoint, recentBizTime } = JSON.parse(
+            res.body || "{}"
+          );
+
+          resolve({
+            orgAddr: address,
+            phone: phoneNos?.[0] || "",
+            latitude: geoPoint?.lat,
+            longitude: geoPoint?.lng,
+            desc: `营业时间：${
+              recentBizTime?.title?.replace(/\n/g, " ") || "-"
+            }`,
+          });
+        } else {
+          console.log("getDzDetail err: ", item.shopUuid, err);
+
+          resolve({
+            orgAddr: "",
+            phone: "",
+            latitude: "",
+            longitude: "",
+            desc: "",
+          });
+        }
+      }
+    );
+  });
+
+/**
+ * 获取大众点评列表， 小程序的headers
+ * @param {*} index
+ * @returns
+ */
+const getDzList = (index = 0) =>
+  new Promise((resolve) => {
+    request.get(
+      `https://m.dianping.com/wxmapi/search?cityId=7&locateCityid=7&categoryId=0&sortId=0&range=-1&lat=22.60956144876099&lng=114.12653115188573&myLat=22.60956144876099&myLng=114.12653115188573&keyword=%E7%AF%AE%E7%90%83%E5%9C%BA&start=${index * 10}`,
+      {
+        headers: {
+          Host: "m.dianping.com",
+          wechatversion: "8.0.34",
+          "content-type": "application/json",
+          channel: "weixin",
+          openidPlt: "oPpUI0darwIbMvZxLeG8CgYWzZHY",
+          sdkversion: "2.30.4",
+          openid: "mWq3fmLuGZDgPhqFgvu_25T8UYXJVaa2VngiSW5_vwY",
+          token:
+            "279aa4474837b29d0d502fb9c43a28652b420bfef2e060dcdf44fed44cbcdfc2b5b744f30169ec7d122c6570b5777b80d539749021b9a3376bf633109e3ee4983cebba92c0c64782a5750269448fee2ca794b5c32b98977c8bb1b8868edfdaa2",
+          platform: "Android",
+          platformversion: "15.5",
+          dpid: "mWq3fmLuGZDgPhqFgvu_25T8UYXJVaa2VngiSW5_vwY",
+          minaname: "dianping-wxapp",
+          minaversion: "9.30.1",
+          channelversion: "8.0.34",
+          Referer:
+            "https://servicewechat.com/wx734c1ad7b3562129/391/page-frame.html",
+          "User-Agent":
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.34(0x18002230) NetType/WIFI Language/zh_CN",
+        },
+      },
+      (err, res) => {
+        if (!err && res.statusCode === 200) {
+          const { data } = JSON.parse(res.body || "{}");
+          console.log("getDzList ok: ", index);
+          resolve(data.list);
+        } else {
+          console.log("getDzList err: ", index, err);
+
+          resolve([]);
+        }
+      }
+    );
+  });
+
 const getDzData = async () => {
-  // filterMtId();
-  const { data } = fs.readJSONSync(srcFile) || {};
-  let res = [];
+  for (let index = 40; index < 60; index++) {
+    const data = (await getDzList(index)) || [];
+    let res = [];
 
-  for (let i = 0; i < data.length; i++) {
-    const item = data[i].shopInfo;
-    item.latitude = item.myLat;
-    item.longitude = item.myLng;
-    const { areaCode, orgAddr } = await parseLocation(item);
-    let note = item.recommendReason?.text || "";
+    for (let i = 0; i < data.length; i++) {
+      const item = data[i].shopInfo;
+      const { orgAddr, phone, latitude, longitude, desc } = await getDzDetail(
+        item
+      );
+      const { areaCode } = await parseLocation({ latitude, longitude });
+      let note = item.recommendReason?.text || "";
 
-    const tags = (item.tagList || []).map((v) => ({
-      value: v.text,
-      type: v.textColor === "#B15E2C" ? 2 : 1,
-    }));
+      const tags = (item.tagList || []).map((v) => ({
+        value: v.text,
+        type: v.textColor === "#B15E2C" ? 2 : 1,
+      }));
 
-    res.push({
-      orgId: item.shopUuid,
-      dzId: item.shopUuid,
-      dzPath: item.navData?.url,
-      areaCode,
-      latitude: item.latitude,
-      longitude: item.longitude,
-      orgName: item.name + item.branchName ? `(${item.branchName})` : "",
-      orgAddr,
-      note,
-      score: +item.starScore,
-      tags: [{ value: `评分：${item.starScore || "-"}`, type: 2 }, ...tags],
-      desc,
-      phone: "",
-    });
+      res.push({
+        orgId: item.shopUuid,
+        dzId: item.shopUuid,
+        dzPath: item.navData?.url,
+        areaCode,
+        latitude: latitude,
+        longitude: longitude,
+        orgName: item.name + (item.branchName ? `（${item.branchName}）` : ""),
+        orgAddr,
+        note,
+        score: +item.starScore,
+        tags: [
+          {
+            value: `评分：${`${item.starScore}`.slice(0, -1) || "-"}`,
+            type: 2,
+          },
+          ...tags,
+        ],
+        desc,
+        phone,
+      });
 
-    console.log("ok: ", { id: item.id, i });
+      console.log("ok: ", { id: item.shopUuid, i, index });
 
-    if (i > 0 && i % 10 === 0) {
-      const text = fs.readJSONSync(dzFile);
-      fs.writeJsonSync(dzFile, [...text, ...res], { spaces: 2 });
-      res = [];
+      await sleep();
     }
 
-    await sleep();
+    const text = fs.readJSONSync(dzFile);
+    fs.writeJsonSync(dzFile, [...text, ...res], { spaces: 2 });
   }
-
-  const text = fs.readJSONSync(dzFile);
-  fs.writeJSON(dzFile, [...text, ...res], { spaces: 2 });
 };
 
 getDzData();
